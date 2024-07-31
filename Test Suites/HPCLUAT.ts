910a412e30827cee20fd82f361441d511e2c26c7
mallika <mallikapk@sternadevices.in>
2024-07-31 12:53:32 +0530

    uat

--- Test Suites/HPCLUAT.ts
+++ Test Suites/HPCLUAT.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>58e694fa-64dc-4522-8bcc-a6eee6f01a7d</testSuiteGuid>
+   <testCaseLink>
+      <guid>63aec3e9-89ad-416d-9ed2-453e6b990545</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/U3UATMain_Modules_Test_case_2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
