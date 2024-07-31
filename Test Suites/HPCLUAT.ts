14028112061336ea57c43547e3ba50c86c48890c
mallika <mallikapk@sternadevices.in>
2024-07-31 13:10:18 +0530

    new

--- Test Suites/HPCLUAT.ts
+++ Test Suites/HPCLUAT.ts
@@ -12,6 +12,13 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>58e694fa-64dc-4522-8bcc-a6eee6f01a7d</testSuiteGuid>
    <testCaseLink>
+      <guid>7e29a06a-272e-433a-a367-6c35c41bebdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/U3UAT_Login_Test_case_1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
       <guid>63aec3e9-89ad-416d-9ed2-453e6b990545</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
